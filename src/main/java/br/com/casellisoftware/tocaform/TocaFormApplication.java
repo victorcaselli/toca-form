acd109d61446a5871ef38e97e71aa66c9b867b05
VictorCaselli <victor.dias@gmail.com>
2021-12-26 11:53:55 -0300

    project created

---
+++ src/main/java/br/com/casellisoftware/tocaform/TocaFormApplication.java
@@ -0,0 +1,13 @@
+package br.com.casellisoftware.tocaform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TocaFormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TocaFormApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/casellisoftware/tocaform/TocaFormApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.casellisoftware.tocaform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TocaFormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
